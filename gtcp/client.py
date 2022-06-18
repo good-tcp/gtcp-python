457ae49d94222d173e7277c135a5bb1451b99d47
Keizou W <keizouw8@gmail.com>
2022-06-18 14:25:22 -0700

    added simple tcp functionality for creating servers/clients and exchanging security information

--- gtcp/client.py
+++ gtcp/client.py
@@ -1,2 +1,29 @@
+from email import message
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+import socket
+from threading import *
+
 class client:
-    pass
+    def __init__(self, conn, callback):
+        self.__s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__s.connect(tuple(conn.split(":")[i] if i == 0 else int(conn.split(":")[i]) for i in range(2)))
+        key = RSA.generate(2048)
+        encryptor = PKCS1_OAEP.new(key)
+        self.__crypto = {"server": 0, "client": (key, encryptor)}
+        self.__s.sendall(key.publickey().exportKey(format='PEM', passphrase=None, pkcs=1))
+        while True:
+            data = self.__s.recv(1024)
+            if not self.__crypto["server"]:
+                serverkey = RSA.importKey(data, passphrase=None)
+                serverencryptor = PKCS1_OAEP.new(serverkey)
+                self.__crypto["server"] = (serverkey, serverencryptor)
+                callback()
+            else:
+                print(str(self.__crypto["client"][1].decrypt(data), 'utf-8'))
+    def emit(self, event, *data):
+        self.__s.sendall(self.__crypto["server"][1].encrypt(bytearray(data[0], 'utf-8')))
+    def on(self, event, callback):
+        pass
+
+c = client("127.0.0.1:42069")
--- gtcp/server.py
+++ gtcp/server.py
@@ -1,2 +1,39 @@
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+from uuid import uuid4
+import socket
+from threading import *
+
 class server:
-    pass
+    def __init__(self, port):
+        self.__s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__s.bind(('127.0.0.1', port))
+        self.__s.listen(1)
+        self.__sockets = {}
+    def connection(self, callback):
+        def startcon():
+            while True:
+                connection, client_address = self.__s.accept()
+                key = RSA.generate(2048)
+                encryptor = PKCS1_OAEP.new(key)
+                socketid = uuid4()
+                self.__sockets[socketid] = {"clientconn": (connection, client_address), "crypto": {"server": (key, encryptor), "client": 0}, "rooms": [socketid]}
+                connection.sendall(key.publickey().exportKey(format='PEM', passphrase=None, pkcs=1))
+                data = connection.recv(1024)
+                if not self.__sockets[socketid]["crypto"]["client"]:
+                    clientkey = RSA.importKey(data, passphrase=None)
+                    clientencryptor = PKCS1_OAEP.new(clientkey)
+                    self.__sockets[socketid]["crypto"]["client"] = (clientkey, clientencryptor)
+                    callback("e")
+                else:
+                    print(self.__sockets[socketid]["crypto"]["server"][1].decrypt(data))
+        t = Thread(target=startcon)
+        t.start()
+    def emit(self, message):
+        for socket in self.__sockets:
+            self.__sockets[socket]["clientconn"][0].sendall(self.__sockets[socket]["crypto"]["client"][1].encrypt(bytearray(message, 'utf-8')))
+
+s = server(42069)
+s.connection(lambda socket:
+    s.emit("test")
+)
